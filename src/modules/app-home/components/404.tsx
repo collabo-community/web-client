47c82c61fabef7cd2a79f4017a68d60353ed90ba
Ifycode <marychord@gmail.com>
2024-04-01 19:21:13 +0100

    Fix: AppHome name

--- src/modules/app-home/components/404.tsx
+++ src/modules/app-home/components/404.tsx
@@ -1,9 +1,9 @@
-import mainApp404 from '@/modules/app-home/styles/modules/custom404.module.css';
+import appHome404 from '@/modules/app-home/styles/modules/custom404.module.css';
 
 export default function Custom404Component() {
   return (
     <>
-      <h1 className={mainApp404.test}>AppHome 404 page content</h1>
+      <h1 className={appHome404.test}>AppHome 404 page content</h1>
     </>
   );
 }
